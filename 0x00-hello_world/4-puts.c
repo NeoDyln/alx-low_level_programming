efae91e14df98c8a8f04a13cb2d3966ebe3c9aaa
NeoDyln <dylankipkoech1@gmail.com>
2022-06-02 06:44:50 -0700

    Attempt 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+ *main - This is the entry point
+ *
+ *return - Exit point. Alsways return 0 (Success)
+ */
 int main(void)
 {/* The function below outputs a text */
 puts("\"Programming is like building a multilingual puzzle");
