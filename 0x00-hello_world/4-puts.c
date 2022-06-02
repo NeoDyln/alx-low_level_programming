7647e41dadc269fb69892baff535fa2af9a5720c
NeoDyln <dylankipkoech1@gmail.com>
2022-06-02 06:58:40 -0700

    Attempt 10

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  *main - Entry point
  *
  *return: Always 0 (Success)
+ *
  */
 int main(void)
-{/* The function below outputs a text */
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
