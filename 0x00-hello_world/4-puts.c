11bf254600bb8603b627a2723ad3c90f82949804
NeoDyln <dylankipkoech1@gmail.com>
2022-06-02 06:38:20 -0700

    Attempt 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {/* The function below outputs a text */
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
