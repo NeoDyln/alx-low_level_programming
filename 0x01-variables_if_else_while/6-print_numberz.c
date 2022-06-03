39409a951232403e4078684bffa5b42e40c979db
NeoDyln <dylankipkoech1@gmail.com>
2022-06-03 10:54:05 -0700

    Some attempt 1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *main - Entrry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i);
+i = i + 1;
+}
+putchar('\n');
+return (0);
+}
