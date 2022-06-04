794448525a64628d4dc3200f40f8e0300ff8e218
NeoDyln <dylankipkoech1@gmail.com>
2022-06-03 22:19:58 -0700

    Attempt 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,27 @@
 int main(void)
 {
 int a[10] = {48,49,50,51,52,53,54,55,56,57};
-int b[10] = {48,49,50,51,52,53,54,55,56,57};
 int sizeA = sizeof (a) / sizeof (a[0]);
-int sizeB = sizeof (b) / sizeof (b[0]);
 int c;
 int d;
-for (c = 0; c < sizeA; c++)
+int e;
+int f;
+for (c = 0; c < sizeA; c++){
+for (d = 0; d < sizeA; d++)
+{
+for (e = 0; e < sizeA; e++)
+{
+for (f = 0; f < sizeA; f++)
 {
 putchar(a[c]);
-for (d = 0; d < sizeB; d++)
-{
-putchar(a[c]);
+putchar(a[d]);
 putchar(' ');
-putchar(a[c]);
-putchar(b[d]);
+putchar(a[e]);
+putchar(a[f]);
 putchar(44);
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
