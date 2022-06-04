d21c1ecc2f477d83eafb689fc3aa821c994bb5be
NeoDyln <dylankipkoech1@gmail.com>
2022-06-03 20:13:28 -0700

    Attempt 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *main - Entrry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int a = 48;
+int b = 48;
+int c;
+while (b < 58)
+{
+c = a;
+while (c < 58)
+{
+if (b != c)
+{
+putchar(b);
+putchar(c);
+if (!(b == 56 && c == 57))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+b = b + 1;
+a = a + 1;
+}
+putchar('\n');
+return (0);
+}
