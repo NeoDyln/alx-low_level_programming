250a3eae59c30af000b1a1732f4bda973ba2ad41
NeoDyln <dylankipkoech1@gmail.com>
2022-06-08 13:51:22 -0700

    Attempt 6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@
 void times_table(void)
 {
 int num1, num2 = 0;
-while (num1 < 10)
+for (num1 = 0; num1 < 10; num1++)
 {
-while (num2 < 10)
+for (num2 = 0; num2 < 10; num2++)
 {
 int prod = num1 * num2;
 if (num1 == 0)
@@ -32,9 +32,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(prod + '0');
 }
-num2++;
 }
-num1++;
 _putchar('\n');
 }
 }
