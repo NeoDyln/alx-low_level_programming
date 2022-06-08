63d8145cf24605bb0b3841ac0fbfa5c86e2264e1
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 06:14:26 +0300

    Attempt 5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - returns 1 if lowercase alphabet and 0 otherwise
- * 
+ * @curLetter: Contains the current letter 
  * Return: 0 for uppercase, 1 for lowercase
  */
 int _islower(int curLetter)
