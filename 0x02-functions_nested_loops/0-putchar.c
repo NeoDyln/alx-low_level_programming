d2e5985d45e67eca99ac6f0ec8880c8875184770
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:55:38 +0300

    Attempt 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
 int main()
 {
 char st[9] = "_putchar";
-for(int indexCounter = 0; indexCounter < 8; indexCounter++)
+for (int indexCounter = 0; indexCounter < 8; indexCounter++)
 {
 _putchar(st[indexCounter]);
 }
