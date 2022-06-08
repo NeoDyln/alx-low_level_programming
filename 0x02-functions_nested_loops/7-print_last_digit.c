754968ee2c13b034338a1d839a8b33518987bcc2
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:47:29 +0300

    Attempt 1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point returns last digit in number
+ * 
+ * Return: Always last digit
+ */
+int print_last_digit(int curNum)
+{
+int lastDig = curNum % 10;
+if (curNum < 0)
+{
+lastDig = lastDig * -1;
+}
+_putchar(lastDig + '0');
+return (lastDig);
+}
