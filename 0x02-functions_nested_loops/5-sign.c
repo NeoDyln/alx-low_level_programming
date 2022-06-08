ebbb66bb8bc9015bac66a8a8b6b09b643e4935c4
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 17:39:09 +0300

    Please work

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- *@signCheck: Contains num representation of letter
+ *@curSign: Contains num representation of letter
  * Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
  */
-int print_sign(int signCheck)
+int print_sign(int curSign)
 {
-if (signCheck > 0)
+if (curSign > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (signCheck == 0)
+else if (curSign == 0)
 {
 _putchar('0');
 return (0);
