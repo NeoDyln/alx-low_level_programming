d703d7fbf5dde257a522c01e87d9d547609ac0ec
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:34:32 +0300

    Create 2-print_alphabet_x10.c
    Attempt 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int loopCounter;
+char curLetter;
+for (loopCounter = 0; loopCounter < 10; loopCounter++)
+{
+for (curLetter = 'a'; curLetter <= 'z'; curLetter++)
+_putchar(c);
+_putchar('\n');
+}
+}
