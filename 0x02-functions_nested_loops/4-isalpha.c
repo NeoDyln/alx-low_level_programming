f215072fa0ac64a015b05bb8353b2718d0e9305b
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:37:47 +0300

    Create 4-isalpha.c
    Attempt 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
+ */
+int _isalpha(int curLetter)
+{
+if ((curLetter >= 'A' && curLetter <= 'Z') || (curLetter >= 'a' && curLetter <= 'z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
