de89e84388c4b148a77cd5f15d9326687fc05379
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 22:55:09 +0300

    Updates to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,8 @@ int print_sign(int curSign);
 int _abs(int curVal);
 int print_last_digit(int curNum);
 void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+void print_to_98(int curNum);
+void print_times_table(int curNum);
 #endif
