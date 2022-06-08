53d1559940a85096ede13cf9cf7298f9d2332935
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:44:06 +0300

    Create 6-abs.c
    Attempt 1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point and returns absolute values
+ *
+ * Return: curVal in absolute form
+ */
+int _abs(int curVal)
+{
+if (curVal < 0)
+{
+return (curVal * -1);
+}
+return (curVal);
+}
