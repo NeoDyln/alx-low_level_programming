bf3bcd0906768ded24831a283237297a799fcfd4
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 23:13:16 +0300

    Attempt 1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @num1: Enters first number
+ * @num2: Enters second number
+ *
+ * Return: Outputs summation
+ */
+int add(int num1, int num2)
+{
+int fSum = num1 + num2;
+return (fSum);
+}
