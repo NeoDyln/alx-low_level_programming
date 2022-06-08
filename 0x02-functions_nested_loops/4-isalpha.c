8622767bc920b0c509b9dcc32338835bb5e75288
NeoDyln <dylankipkoech1@gmail.com>
2022-06-08 07:36:36 -0700

    Please work

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- *@curLetter: COntains current letter
+ *@curLet: COntains current letter
  * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
  */
-int _isalpha(int curLetter)
+int _isalpha(int curLet)
 {
-if ((curLetter >= 'A' && curLetter <= 'Z') || (curLetter >= 'a' && curLetter <= 'z'))
+if ((curLet >= 'A' && curLet <= 'Z') || (curLet >= 'a' && curLet <= 'z'))
 {
 return (1);
 }
