4874d914bd582391a082289c0df8a56290f950c5
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:39:38 +0300

    Create 5-sign.c
    Attempt 1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
+ */
+int print_sign(int signCheck)
+{
+if (signCheck > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (signCheck == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
