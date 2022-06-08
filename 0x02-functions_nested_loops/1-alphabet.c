1c7ceca9ddce1acd25975dad854e2fd9e6ab2875
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 05:32:03 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ **/
+void print_alphabet(void)
+{
+char curLetter;
+for (curLetter = 'a'; curLetter <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
