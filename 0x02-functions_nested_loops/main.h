0b824e6774120b34ead6a8d5ff3a451bdc878a0e
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-08 17:41:52 +0300

    Editing 6

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet_x10(void);
 int _islower(int curLetter);
 int _isalpha(int curLet);
 int print_sign(int curSign);
-int _abs(int);
+int _abs(int curVal);
 int print_last_digit(int);
 void jack_bauer(void);
 #endif
