b1a6054cd0b23f2de8e1a8a97452789e0d745fea
NeoDyln <dylankipkoech1@gmail.com>
2022-06-08 18:50:13 -0700

    Attempt 25

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -  Shows first 50 Fibonacci numbers start end
+ * main -  Shows first 50 Fibonacci numbers start 1 end 5oth
  * @void: no argument
  * Return: 0
  */
 int main(void)
 {
 long num1 = 1;
-long num2 = 2; 
+long num2 = 2;
 long sumFib;
 printf("%d, %d", num1, num2);
 while (sumFib < 20365011073)
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - 
+ * main - 98 fibonacci nums
  *
  * Return: 0
  */
