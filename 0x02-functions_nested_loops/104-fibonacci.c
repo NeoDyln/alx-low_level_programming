712cbc921819eef527c853b681ab9cadd56ea4c9
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-09 04:21:13 +0300

    Up 1

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int fibCt = 2;
+float num1 = 1;
+float num2 = num1 + 1;
+float num3 = num1 + num2;
+printf("%.0f, ", num1);
+printf("%.0f, ", num2);
+while (fibCt < 98)
+{
+printf("%.0f", num3);
+num1 = num2;
+num2 = num3;
+num3 = num1 + num2;
+if (fibCt++ < 98)
+{
+printf(", ");
+}
+fibCt++;
+}
+printf("\n");
+return (0);
+}
