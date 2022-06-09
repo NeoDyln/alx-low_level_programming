9a24f9fcb7a5b5ffa4096b8d8341106409486053
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-09 04:10:11 +0300

    Up 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - 
+ * Description: Outputs first 50 fibonacci numbers starting with 1 and 2.
+ * Return: 0
+ */
+int main(void)
+{
+int fibCt = 2;
+long int num1 = 1; 
+long int num2 = 2;
+long int numNxtTrack;
+printf("%lu, ", num1);
+while (fibCt <= 50)
+{
+if (fibCt == 50)
+{
+printf("%lu\n", num2);
+}
+else
+{
+printf("%lu, ", num2);
+}
+numNxtTrack = num2;
+num2 += num1;
+num1 = numNxtTrack;
+count++;
+}
+return (0);
+}
