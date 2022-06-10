7cbede5ffdc4e1c32b01b46e000375810699cb3c
NeoDyln <dylankipkoech1@gmail.com>
2022-06-10 14:19:09 -0700

    Teste

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,10 @@ if (month == 2 && day >= 60)
 {
 printf("Invalid Date: %02d/%02d/%04d\n", month, day - 31, year);
 }
+else
+{
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
