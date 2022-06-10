1fa7e462eebcd0a4a3e659187e4ae9982a2493e6
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-10 05:23:16 +0300

    Up 1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: multiples of 3, print fizz, of 5, buzz, of both 
+ * Return: void
+ */
+
+int main(void)
+{
+
+int count = 100;
+int i;
+
+i = 1;
+while (i <= count)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+
+else if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz ");
+}
+
+else if (i % 5 == 0 && i % 3 != 0)
+{
+if (i < count)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+{
+printf("%i ", i);
+}
+
+i++;
+
+
+}
+printf("\n");
+return (0);
+}
