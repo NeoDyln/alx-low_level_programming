1fd3917cca7062ebcff784149dfaf0086a48074d
NeoDyln <dylankipkoech1@gmail.com>
2022-06-10 12:22:58 -0700

    Test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,15 @@ void more_numbers(void)
 int counter, numCur;
 for (counter = 0; counter < 10; counter++)
 {
-for (numCur = 0; numCur < 15; numCur++)
+for (numCur = 48; numCur < 63; numCur++)
 {
+int numRes = numCur;
 if (numCur > 9)
 {
 _putchar(49);
+numRes = numRes - 10;
 }
-int valM = curNum % 10;
-_putchar(valM + '0');
+_putchar(numRes);
 }
 _putchar('\n');
 }
