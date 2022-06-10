64a256782d0af446d43bb961e0c99ede7cf5de64
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-10 05:17:36 +0300

    Up

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line
+ *@n: 
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+
+int hor;
+int ver;
+
+if (n > 0)
+{
+for (hor = 0; hor < n; hor++)
+{
+for (ver = 0; ver < i; ver++)
+{
+_putchar (' ');
+}
+_putchar (92);
+_putchar ('\n');
+
+}
+
+}
+
+
+else
+{
+_putchar ('\n');
+}
+
+}
