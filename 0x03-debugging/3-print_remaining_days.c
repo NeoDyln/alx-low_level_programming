6cf464e726c38fc182a4ab52fb26cb692b36eeed
NeoDyln <dylankipkoech1@gmail.com>
2022-06-10 00:11:53 -0700

    Attempt 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -28,12 +28,9 @@ else if (year % 4 != 0 || year % 4 != 0)
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+day--;
 }
-else
-{
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-}
 }
 }
