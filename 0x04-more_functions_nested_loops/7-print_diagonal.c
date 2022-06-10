0d229a7131c7d6b5c1da32818c5fd65a661e9940
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-10 05:46:21 +0300

    up 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,29 +6,18 @@
  */
 void print_diagonal(int n)
 {
+	int i = 0, j;
 
-int hor;
-int ver;
-
-if (n > 0)
-{
-for (hor = 0; hor < n; hor++)
-{
-for (ver = 0; ver < i; ver++)
-{
-_putchar(' ');
-}
-_putchar(92);
-_putchar('\n');
-
-}
-
-}
-
-
-else
-{
-_putchar('\n');
-}
-
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
