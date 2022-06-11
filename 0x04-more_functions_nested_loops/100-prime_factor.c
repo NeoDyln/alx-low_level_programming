03ccae5ffb06096bb244aeba0daf04de9092a4d2
NeoDyln <dylankipkoech1@gmail.com>
2022-06-10 18:58:07 -0700

    Test 5

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,28 +3,25 @@
 /**
  *main - Entry point
  *
- *Return: Always 
+ *Return: Always 0
  */
 
 int main(void)
 {
-	long int prime = 612852475143;
-	long int quotient = prime;
-	long int divisor = 2;
-
-	while (quotient != divisor)
-	{
-		if (quotient % divisor == 0)
-		{
-			quotient = quotient / divisor;
-		}
-		else
-		{
-			divisor++;
-		}
-	}
-
-	printf("%li\n", quotient);
-
-	return (0);
+long int prime = 612852475143;
+long int quotient = prime;
+long int divisor = 2;
+while (quotient != divisor)
+{
+if (quotient % divisor == 0)
+{
+quotient = quotient / divisor;
+}
+else
+{
+divisor++;
+}
+}
+printf("%li\n", quotient);
+return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,34 +3,30 @@
 /**
  *print_number - Prints an integer
  *@n: number to be printed
- *Return: Voi
+ *Return: Void
  */
 
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+unsigned int numCheck, d, count;
+if (n < 0)
+{
+_putchar(45);
+numCheck = n * -1;
+}
+else
+{
+numCheck = n;
+}
+d = numCheck;
+count = 1;
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+_putchar(((numCheck / count) % 10) + 48);
+}
 }
