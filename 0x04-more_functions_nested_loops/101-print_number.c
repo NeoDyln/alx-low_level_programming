c46f6d25fde222c89b575c620890fe86b618858d
NeoDyln <dylankipkoech1@gmail.com>
2022-06-10 18:33:58 -0700

    Test 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,18 +17,18 @@ _putchar(45);
 if (n / 1000 > 0)
 {
 dig1 = n / 1000;
-_putchar(dig1 + '0');
+_putchar(dig1 + '/');
 }
 if (n / 100 > 0)
 {
 dig2 = n / 100;
-_putchar(dig2 + '0');
+_putchar(dig2 + '/');
 }
 if (n / 10 > 0)
 {
 dig3 = n / 10;
-_putchar(dig3 + '0');
+_putchar(dig3 + '/');
 }
-_putchar(n % 10 + '0');
+_putchar(n % 10 + '/');
 _putchar('\n');
 }
