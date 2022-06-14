434033e48ddae2c17add7bf9a5b7a8269c722377
NeoDyln <dylankipkoech1@gmail.com>
2022-06-13 19:55:59 -0700

    Attempt 1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * rev_string - a function that reverse a string
- * followed by a new line.
+ * rev_string - a function to reverse a string
  * @s: an input string
  * Return: Nothing
  */
 void rev_string(char *s)
 {
-	int len = 0, i = 0;
-	char aux;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len--)
-	{
-		aux = s[i];
-		s[i++] = s[len];
-		s[len] = aux;
-	}
+int len = 0, i = 0;
+char aux;
+while (s[len] != '\0')
+{
+len++;
+while (i < len--)
+{
+aux = s[i];
+s[i++] = s[len];
+s[len] = aux;
+}
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -15,4 +17,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endi
+#endif
