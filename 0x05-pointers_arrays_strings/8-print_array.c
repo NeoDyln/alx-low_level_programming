fa47fe1fef719b2fafc9a5858470633691bfde08
NeoDyln <dylankipkoech1@gmail.com>
2022-06-14 10:03:33 -0700

    Attempt 2.2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,12 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
-	for (; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i < n - 1)
-			printf(", ");
-	}
-	putchar('\n');
+int i = 0;
+for (; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
+putchar('\n');
 }
