fc0913388bdae75c75fac62058323401bfce3c4b
NeoDyln <dylankipkoech1@gmail.com>
2022-06-16 16:38:42 +0000

    Test 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,24 +9,27 @@
 
 char *_strcat(char *dest, char *src)
 {
-int destC = 0;
+int destC;
+destC = 0;
 while (dest[destC] != '\0')
 {
 destC++;
 }
+/* Here we count the number of chars in src inclusive of the \0  */
 int srcC = 0;
 while (src[srcC] != '\0')
 {
 srcC++;
 }
-dest[destC] = '0';
-int destL = destC;
-int destU = destC + srcC;
-for (; destC < destU; destC++)
+/**
+ * Now we loop through the values of the src array adding each character to the dest array  
+ * We'll also initialize a new variable to help track the count
+ */
+int srcU = 0;
+for (srcU = 0; srcU < srcC && src[srcU] != '\0'; srcU++)
 {
-if (destC == destL)
-{
-
+dest[destC + srcU] = src[srcU];
 }
-}
+destC = destC + srcU;
+dest[destC] = '\0';
 }
