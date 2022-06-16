0745ea8264895677d3ea908b47b4b6b9cd6040f0
NeoDyln <dylankipkoech1@gmail.com>
2022-06-16 18:11:14 +0000

    Attempt 5

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *_strcat - Concantenates two strings
- *@dest: End string to append
- *@src: String to append
- *Return: pointer to dest
+ * *_strcat - Concantenates two strings
+ * @dest: End string to append
+ * @src: String to append
+ *
+ * Description: A cocantenator
+ * Return: pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,14 +15,16 @@ char *_strcat(char *dest, char *src)
 int destC = strlen(dest);
 /* This counts the number of chars in src array  */
 int srcC = strlen(src);
-/* This acts as a loop count for the addition of chars to dest from src until before the \0  */
-int srcU = 0;
 /**
- * Now we loop through the values of the src array adding each character to the dest array  
- * We'll also initialize a new variable to help track the count
+ * This acts as a loop count for the addition
+ * of chars to dest from src until before the break
+ *
+ * Now we loop through the values of the src
+ * array adding each character to the dest array
+ * We'll also initialize a new variable to help
+ * track the count
  */
-
-for (;srcU < srcC && src[srcU] != '\0'; srcU++)
+for (int srcU = 0; srcU < srcC && src[srcU] != '\0'; srcU++)
 {
 dest[destC + srcU] = src[srcU];
 }
