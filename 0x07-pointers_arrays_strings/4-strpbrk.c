825a3d0041ed201b75ae61007252584a5db6a984
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-06-21 05:20:32 +0300

    up 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - matches any character specified
+ * @s: Checked string
+ * @accept: Counter check
+ * Return: string s that matches any character specified in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j, k = 0;
+
+	for (k = 0; s[k] != '\0'; k++) 
+	{
+		for (j = 0; accept[j] != '\0'; j++)  
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+}
