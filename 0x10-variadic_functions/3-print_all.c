a53b81d9e3d58147f887dd380f3649e2ad86f3c6
NeoDyln <dylankipkoech1@gmail.com>
2022-07-08 03:21:02 +0000

    Betty manze why

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			}checkD++;
+			} checkD++;
 		}
 	switch (format[checkF])
 	{
@@ -45,6 +45,6 @@ void print_all(const char * const format, ...)
 			}
 		printf("%s", str);
 		break;
-	}checkF++;
+	} checkF++;
 	}
 }
