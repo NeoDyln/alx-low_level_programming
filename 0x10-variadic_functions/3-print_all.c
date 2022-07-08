f12602c79384adece2010b79e1dbf9e5ef8649d2
Dylan Rono <42619900+NeoDyln@users.noreply.github.com>
2022-07-08 06:41:44 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,35 +15,33 @@ void print_all(const char * const format, ...)
 	va_start(allList, format);
 	while (format[checkF])
 	{
-		while (dataT[checkD] != '\0')
-		{
-			if (format[checkF] == dataT[checkD] && c == 1)
-			{
-				printf(", ");
-				break;
-			} checkD++;
-		}
 		switch (format[checkF])
 		{
 			case 'c':
-				printf("%c", va_arg(allList, int)), c = 1;
+				printf("%c", va_arg(allList, char));
+				printf(", ");
 				break;
 			case 'i':
-				printf("%d", va_arg(allList, int)), c = 1;
+				printf("%d", va_arg(allList, int));
+				printf(", ");
 				break;
 			case 'f':
-				printf("%f", va_arg(allList, double)), c = 1;
+				printf("%f", va_arg(allList, double));
+				printf(", ");
 				break;
 			case 's':
-				str = va_arg(allList, char *); c = 1;
+				str = va_arg(allList, char *);
 				if (!str)
 				{
 					printf("(nil)");
+					printf(", ");
 					break;
 				}
 			printf("%s", str);
+			printf(", ");
 			break;
-		} checkF++;
+		}
+		checkF++;
 	}
 	printf("\n");
 }
