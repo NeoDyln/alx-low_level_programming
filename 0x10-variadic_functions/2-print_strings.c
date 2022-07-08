f4d619f3ce5d26b0b4bd652aa50ed5134cf7f820
NeoDyln <dylankipkoech1@gmail.com>
2022-07-08 02:59:49 +0000

    All

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings passed
+ * @separator: Value printed between strings
+ * @n: number of strings to be printed
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strList;
+	unsigned int count = 0;
+	char *ranStr;
+	
+	va_start(strList, n);
+	for (count = 0; count < n; count++)
+	{
+		ranStr = va_arg(strList, char *);
+		if (ranStr == NULL)
+			printf("(nil)");
+		
+		else
+			printf("%s", ranStr);
+		
+		if (separator != NULL && count < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(strList);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,60 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints any data types of all passed in args
+ * @format: Data types allowed
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list allList;
+	int checkF, checkD, c = 0;
+	char dataT[5] = "cifs";
+
+	va_start(allList, format);
+
+	while (format && format[checkF])
+	{
+		/* Above we checked if the data formats are passed. If so, check if checkF'th value exists */
+		/* We'll increment the value at every stage in the loop */
+
+		while (dataT && dataT[checkD])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			}
+			checkD++;
+
+			/* Above we check if actual data formats exist. If so, check if checkD'th value exists */
+			/* We'll increment the value at every stage in the loop */
+			switch (format[checkF])
+			{
+				case 'c':
+					printf("%c", va_arg(allList, int)), c = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(allList, int)), c = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(alList, double)), c = 1;
+					break;
+				case 's':
+					str = va_arg(allList, char *), c = 1;
+
+					if (!str)
+					{
+						printf("(nil)");
+						break;
+					}
+				printf("%s", str);
+				break;
+																								}
+			
+
+		checkF++;
+	}
+}
