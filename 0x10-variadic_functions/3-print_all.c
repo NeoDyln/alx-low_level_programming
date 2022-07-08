c002a1445546f67a60b393ee86e29f13946c5ed5
NeoDyln <dylankipkoech1@gmail.com>
2022-07-08 03:29:32 +0000

    Betty + compile errors waah

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,39 +11,39 @@ void print_all(const char * const format, ...)
 {
 	va_list allList;
 	int checkF, checkD, c = 0;
-	char dataT[5] = "cifs";
+	char *str, dataT[5] = "cifs";
 	va_start(allList, format);
-	while (format && format[checkF])
+	while (format[checkF])
 	{
-		while (dataT && dataT[checkD])
+		while (dataT[checkD])
 		{
-			if (format[i] == t_arg[j] && c)
+			if (format[checkF] == dataT[checkD] && c)
 			{
 				printf(", ");
 				break;
 			} checkD++;
 		}
-	switch (format[checkF])
-	{
-		case 'c':
-			printf("%c", va_arg(allList, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(allList, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(alList, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(allList, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
+		switch (format[checkF])
+		{
+			case 'c':
+				printf("%c", va_arg(allList, int)), c = 1;
 				break;
-			}
-		printf("%s", str);
-		break;
-	} checkF++;
+			case 'i':
+				printf("%d", va_arg(allList, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(allList, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(allList, char *); c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+			printf("%s", str);
+			break;
+		} checkF++;
 	}
 	printf("\n");
 }
